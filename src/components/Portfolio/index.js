b738fed5b9218e17e60bee6e30bada940b8e32c7
Yacine Lyoubi <yacinelyoubi37@gmail.com>
2021-10-28 21:17:31 +0200

    deploy on phone

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import style from "./index.module.css";
 
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  return <div className={style.container}>Portfolio</div>;
 };
 
 export default Portfolio;
