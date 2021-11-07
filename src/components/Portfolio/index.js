c531fb51147130e8ae95898bbaf17376eb1b77e7
Yacine Lyoubi <yacinelyoubi37@gmail.com>
2021-11-07 23:36:07 +0100

    portfolio a faire

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,22 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./index.module.css";
 
 const Portfolio = () => {
+  let [isvisible, setIsvisible] = useState(true);
+
+  setTimeout(() => {
+    setIsvisible(true);
+  }, 5000);
   return (
-    <main className={style.main}>
-      <section className={style.section1}>
-        <h1>If you wanna discover my works click or scroll down </h1>
-        <a href="https://portfolioyacinelyoubi.gatsbyjs.io/">
-          All the magic happend here ;)
-        </a>
-        {/* <img
-          className={style.scroll}
-          draggable={false}
-          src="/images/scroll-down.gif"
-          alt="scroll down"
-        /> */}
-      </section>
-    </main>
+    <>
+      <div class={style.wrap}>
+        <div>A</div>
+        <div>B</div>
+        <div>C</div>
+        <div>D</div>
+        <div>E</div>
+      </div>
+    </>
   );
 };
 
--- src/view/Porfolio.js
+++ src/view/Porfolio.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
 import Portfolio from "../components/Portfolio";
 import Footer from "../components/Footer";
+import Loader from "../components/Loader";
 
 const Portolio = () => {
-  return (
-    <>
-      <Header />
-      <Portfolio />
-      <Footer />
-    </>
-  );
+  let [showPortfolio, setShowPortfolio] = useState(false);
+
+  setTimeout(() => {
+    setShowPortfolio(true);
+  }, 5000);
+
+  if (showPortfolio) {
+    return (
+      <>
+        <Header />
+        <Portfolio />
+        <Footer />
+      </>
+    );
+  } else {
+    return <Loader />;
+  }
 };
 
 export default Portolio;
