833ae1311752f41f27e6ce3c3f2ea02bc4e72890
Yacine Lyoubi <yacinelyoubi37@gmail.com>
2021-11-11 17:22:13 +0100

    link first projet

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,16 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import style from "./index.module.css";
 
 const Portfolio = () => {
-  let [isvisible, setIsvisible] = useState(true);
-
-  setTimeout(() => {
-    setIsvisible(true);
-  }, 5000);
   return (
     <>
       <div class={style.wrap}>
-        <div>A</div>
+        <div>
+          <h1>PROJET METEO JS VANILLA</h1>
+
+          <a href="https://github.com/yacine-21/Projet_Meteo_JS_Vanilla">
+            Github
+          </a>
+          <a href="https://projet-meteo-js-vanilla.vercel.app/">Projet</a>
+        </div>
         <div>B</div>
         <div>C</div>
         <div>D</div>
