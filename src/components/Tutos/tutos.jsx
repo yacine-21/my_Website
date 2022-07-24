400494e615d21510eb495b9d5e07d4c120b9c987
yacine-21 <yacinelyoubi37@gmail.com>
2022-07-24 14:16:34 +0200

    feat:add style video card

--- src/components/Tutos/tutos.jsx
+++ src/components/Tutos/tutos.jsx
@@ -1,53 +1,32 @@
 import React from "react";
+import CardVideo from "../CardVideo/CardVideo";
 import style from "./index.module.css";
 
 const Tutos = () => {
 	return (
-		<>
-			<div className={style.wrap}>
-				<div className='max-w-sm  bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700'>
-					<a href='#'>
-						<iframe
-							className='w-full h-full'
-							src='https://www.youtube.com/embed/C8hsuX_uDU0'
-							title='YouTube video player'
-							frameborder='0'
-							allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-							allowfullscreen
-						></iframe>
-					</a>
-					<div className='p-5'>
-						<a href='#'>
-							<h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
-								Linux Intro 1
-							</h5>
-						</a>
-						<p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>
-							Introduction to the basic commands of Linux and how to use them.
+		<div className={style.wrap}>
+			<section className={style.section}>
+				<h1 className={style.Title}>My tutorials</h1>
+				<div className='max-w-4xl p-4 text-gray-800 bg-white rounded-lg shadow hover:bg-gray-500 hover:text-white'>
+					<div class='mb-2'>
+						<div class='h-3 text-3xl text-left text-gray-600'>“</div>
+						<p class='px-4 text-center text-black text-2xl hover:text-white'>
+							Sharing knowledge is the best way to learn
 						</p>
-						<a
-							href='https://www.youtube.com/channel/UCrI2s8VG2F_bYvLhbl0KpfQ/videos'
-							className='inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
-						>
-							View more
-							<svg
-								aria-hidden='true'
-								className='ml-2 -mr-1 w-4 h-4'
-								fill='currentColor'
-								viewBox='0 0 20 20'
-								xmlns='http://www.w3.org/2000/svg'
-							>
-								<path
-									fill-rule='evenodd'
-									d='M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z'
-									clip-rule='evenodd'
-								></path>
-							</svg>
-						</a>
+						<div class='h-3 text-3xl text-right text-gray-600'>”</div>
 					</div>
 				</div>
+			</section>
+			<div className='container_video'>
+				<CardVideo
+					description={
+						"Introduction to the basic commands of Linux and how to use them."
+					}
+					title={"Linux Intro 1"}
+					url={"https://www.youtube.com/embed/C8hsuX_uDU0"}
+				/>
 			</div>
-		</>
+		</div>
 	);
 };
 
