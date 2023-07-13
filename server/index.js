87faf7619c9d4e0477d81e92f4455d54419981c8
yacine-21 <yacinelyoubi37@gmail.com>
2023-07-13 19:27:32 +0200

    first commit

---
+++ server/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+require('dotenv').config()
+app.use(express.json()); // Add this line to parse the request body as JSON
+
+app.post("/send-email", async (req, res) => {
+    console.log(req.body);
+  
+    try {
+      const transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+          user: process.env.REACT_APP_MAIL,
+          pass: process.env.REACT_APP_PASS
+        }
+      });
+  
+      const { from_name, reply_to, message } = req.body;
+      const subject = `Demande de connexion from ${reply_to}`;
+      const text = `Sender Name: ${from_name}\nSender Email: ${reply_to}\n\n${message}`;
+  
+      const info = await transporter.sendMail({
+        from: process.env.REACT_APP_FROM,
+        to: process.env.REACT_APP_FROM,
+        subject: subject,
+        text: text
+      });
+  
+      console.log("Message sent: %s", info.messageId);
+      res.send("Email sent successfully");
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("An error occurred while sending the email");
+    }
+  });
+  
+
+const PORT = process.env.REACT_APP_PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -3,214 +3,202 @@ import { useFormik } from "formik";
 import emailjs from "emailjs-com";
 import * as Yup from "yup";
 import style from "./index.module.css";
-
+import axios from "axios";
 const Contact = () => {
-  const formik = useFormik({
-    initialValues: {
-      from_name: "",
-      lastName: "",
-      reply_to: "",
-      message: "",
-      to_name: "Yacine",
-    },
-    validationSchema: Yup.object({
-      from_name: Yup.string()
-        .min(5, "Must be 5 characters long at least")
-        .max(15, "Must be 15 characters or less")
-        .required("Required"),
-      lastName: Yup.string()
-        .min(5, "Must be 5 characters long at least")
-        .max(20, "Must be 20 characters or less")
-        .required("Required"),
-      reply_to: Yup.string()
-        .email("Invalid email address")
-        .required("Required"),
-      message: Yup.string()
-        .min(5, "Must be 5 characters long at least")
-        .required("Required"),
-    }),
-    onSubmit: (values) => {
-      emailjs
-        .send(
-          `${process.env.REACT_APP_SERVICE_ID_EMAILJS}`,
-          `${process.env.REACT_APP_TEMPLATE_ID_EMAILJS}`,
-          values,
-          `${process.env.REACT_APP_USER_ID_EMAIL_JS}`
-        )
-        .then(
-          (result) => {
-            console.log(result.text);
-            alert("message sent");
-            formik.resetForm();
-          },
-          (error) => {
-            console.log(error.text);
-          }
-        );
-    },
-  });
+	const formik = useFormik({
+		initialValues: {
+			from_name: "",
+			lastName: "",
+			reply_to: "",
+			message: "",
+			to_name: "Yacine"
+		},
+		validationSchema: Yup.object({
+			from_name: Yup.string()
+				.min(5, "Must be 5 characters long at least")
+				.max(15, "Must be 15 characters or less")
+				.required("Required"),
+			lastName: Yup.string()
+				.min(5, "Must be 5 characters long at least")
+				.max(20, "Must be 20 characters or less")
+				.required("Required"),
+			reply_to: Yup.string().email("Invalid email address").required("Required"),
+			message: Yup.string()
+				.min(5, "Must be 5 characters long at least")
+				.required("Required")
+		}),
+		onSubmit: async values => {
+			try {
+        const url = "http://localhost:3000/send-email"
+        const options = {
+          headers: {'Content-Type': 'application/json'}
+        };
 
-  return (
-    <section className={style.section_contact}>
-      <div className={style.container_contact}>
-        <div className={style.bloc_text}>
-          <img alt="me" src="/images/me2.png" className={style.bitmoji} />
-          <h1 className={style.contactUs}>Contact Me</h1>
-          <p className={style.p_contact}>
-            If you want to discuss about anything don't hesitate and send me a
-            message!
-            <br />
-            Fill in the form down here :)
-            <br />
-            <span className={style.emojiBloc}>
-              <img
-                className={style.emoji}
-                src="/images/icons/hand.svg"
-                alt="hand"
-              />
-              <img
-                className={style.emoji}
-                src="/images/icons/hand.svg"
-                alt="hand"
-              />
-            </span>
-          </p>
-        </div>
+        const response = await axios.post(url, values,options);
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+			}
+		}
+	});
 
-        <div className={style.sousBlocInput}>
-          <div className={style.sousBlocInputFlex}>
-            <form onSubmit={formik.handleSubmit}>
-              {formik.touched.from_name && formik.errors.from_name ? (
-                <div>{formik.errors.from_name}</div>
-              ) : null}
+	return (
+		<section className={style.section_contact}>
+			<div className={style.container_contact}>
+				<div className={style.bloc_text}>
+					<img alt='me' src='/images/me2.png' className={style.bitmoji} />
+					<h1 className={style.contactUs}>Contact Me</h1>
+					<p className={style.p_contact}>
+						If you want to discuss about anything don't hesitate and send me a
+						message!
+						<br />
+						Fill in the form down here :)
+						<br />
+						<span className={style.emojiBloc}>
+							<img className={style.emoji} src='/images/icons/hand.svg' alt='hand' />
+							<img className={style.emoji} src='/images/icons/hand.svg' alt='hand' />
+						</span>
+					</p>
+				</div>
 
-              <div className={style.blocInput}>
-                <label className={style.labelInput} htmlFor="from_name">
-                  First Name
-                </label>
-                <input
-                  placeholder="yacine . . ."
-                  className={style.input_contact}
-                  id="from_name"
-                  name="from_name"
-                  type="text"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.from_name}
-                />
-              </div>
-              {formik.touched.lastName && formik.errors.lastName ? (
-                <div>{formik.errors.lastName}</div>
-              ) : null}
+				<div className={style.sousBlocInput}>
+					<div className={style.sousBlocInputFlex}>
+						<form onSubmit={formik.handleSubmit}>
+							{formik.touched.from_name && formik.errors.from_name ? (
+								<div>{formik.errors.from_name}</div>
+							) : null}
 
-              <div className={style.blocInput}>
-                <label className={style.labelInput} htmlFor="lastName">
-                  Last Name
-                </label>
-                <input
-                  placeholder="lyoubi . . ."
-                  className={style.input_contact}
-                  id="lastName"
-                  name="lastName"
-                  type="text"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.lastName}
-                />
-              </div>
+							<div className={style.blocInput}>
+								<label className={style.labelInput} htmlFor='from_name'>
+									First Name
+								</label>
+								<input
+									placeholder='yacine . . .'
+									className={style.input_contact}
+									id='from_name'
+									name='from_name'
+									type='text'
+									onChange={formik.handleChange}
+									onBlur={formik.handleBlur}
+									value={formik.values.from_name}
+								/>
+							</div>
+							{formik.touched.lastName && formik.errors.lastName ? (
+								<div>{formik.errors.lastName}</div>
+							) : null}
 
-              {formik.touched.reply_to && formik.errors.message ? (
-                <div>{formik.errors.reply_to}</div>
-              ) : null}
-              <div className={style.blocInput}>
-                <label className={style.labelInput} htmlFor="reply_to">
-                  Email Address
-                </label>
-                <input
-                  placeholder="yacinelyoubi_tech@protonmail.com . . ."
-                  className={style.input_contact}
-                  id="reply_to"
-                  name="reply_to"
-                  type="email"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.reply_to}
-                />
-              </div>
-              {formik.touched.message && formik.errors.message ? (
-                <div>{formik.errors.message}</div>
-              ) : null}
-              <div className={style.blocInput}>
-                <label className={style.labelInput} htmlFor="message">
-                  Message
-                </label>
-                <textarea
-                  placeholder="hey, I want to connect with you to discuss about . . ."
-                  className={style.textAreaContact}
-                  id="message"
-                  name="message"
-                  type="text"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values.message}
-                />
-              </div>
-              <div className={style.containerInput}>
-                <button className={style.buttonContact} type="submit">
-                  Submit
-                </button>
-              </div>
-              <div className={style.textInfoContainer}>
-                <p>yacinelyoubi_tech@protonmail.com </p>
-                 <h3><a href="https://bio.link/yacine_lyoubi" target="_blank">All my links</a></h3>
-                <span className={style.iconBloc}>
-                  <a
-                    href="https://twitter.com/Yacine_D_21"
-                    className={style.item_footer}
-                    target="_blank"
-                    rel="noreferrer"
-                    aria-label="twitter"
-                  >
-                    <img
-                      className={style.images_footer}
-                      src="/images/icons/twitter.svg"
-                      alt="twitter"
-                    />
-                  </a>
+							<div className={style.blocInput}>
+								<label className={style.labelInput} htmlFor='lastName'>
+									Last Name
+								</label>
+								<input
+									placeholder='lyoubi . . .'
+									className={style.input_contact}
+									id='lastName'
+									name='lastName'
+									type='text'
+									onChange={formik.handleChange}
+									onBlur={formik.handleBlur}
+									value={formik.values.lastName}
+								/>
+							</div>
 
-                  <a
-                    href="https://www.linkedin.com/in/yacine-lyoubi/"
-                    className={style.item_footer}
-                    target="_blank"
-                    rel="noreferrer"
-                    aria-label="linkedin"
-                  >
-                    <img
-                      src="/images/icons/linkedin.svg"
-                      alt="linkedin"
-                      className={style.images_footer}
-                    />
-                  </a>
-                  <a
-                    href="https://github.com/yacine-21"
-                    className={style.item_footer}
-                    target="_blank"
-                    rel="noreferrer"
-                    aria-label="Github"
-                  >
-                    <img
-                      src="/images/icons/github.svg"
-                      alt="github"
-                      className={style.images_footer}
-                    />
-                  </a>
-                </span>
-              </div>
-            </form>
-          </div>
-        </div>
+							{formik.touched.reply_to && formik.errors.message ? (
+								<div>{formik.errors.reply_to}</div>
+							) : null}
+							<div className={style.blocInput}>
+								<label className={style.labelInput} htmlFor='reply_to'>
+									Email Address
+								</label>
+								<input
+									placeholder='yacinelyoubi_tech@protonmail.com . . .'
+									className={style.input_contact}
+									id='reply_to'
+									name='reply_to'
+									type='email'
+									onChange={formik.handleChange}
+									onBlur={formik.handleBlur}
+									value={formik.values.reply_to}
+								/>
+							</div>
+							{formik.touched.message && formik.errors.message ? (
+								<div>{formik.errors.message}</div>
+							) : null}
+							<div className={style.blocInput}>
+								<label className={style.labelInput} htmlFor='message'>
+									Message
+								</label>
+								<textarea
+									placeholder='hey, I want to connect with you to discuss about . . .'
+									className={style.textAreaContact}
+									id='message'
+									name='message'
+									type='text'
+									onChange={formik.handleChange}
+									onBlur={formik.handleBlur}
+									value={formik.values.message}
+								/>
+							</div>
+							<div className={style.containerInput}>
+								<button className={style.buttonContact} type='submit'>
+									Submit
+								</button>
+							</div>
+							<div className={style.textInfoContainer}>
+								<p>yacinelyoubi_tech@protonmail.com </p>
+								<h3>
+									<a href='https://bio.link/yacine_lyoubi' target='_blank'>
+										All my links
+									</a>
+								</h3>
+								<span className={style.iconBloc}>
+									<a
+										href='https://twitter.com/Yacine_D_21'
+										className={style.item_footer}
+										target='_blank'
+										rel='noreferrer'
+										aria-label='twitter'
+									>
+										<img
+											className={style.images_footer}
+											src='/images/icons/twitter.svg'
+											alt='twitter'
+										/>
+									</a>
 
-        {/* 
+									<a
+										href='https://www.linkedin.com/in/yacine-lyoubi/'
+										className={style.item_footer}
+										target='_blank'
+										rel='noreferrer'
+										aria-label='linkedin'
+									>
+										<img
+											src='/images/icons/linkedin.svg'
+											alt='linkedin'
+											className={style.images_footer}
+										/>
+									</a>
+									<a
+										href='https://github.com/yacine-21'
+										className={style.item_footer}
+										target='_blank'
+										rel='noreferrer'
+										aria-label='Github'
+									>
+										<img
+											src='/images/icons/github.svg'
+											alt='github'
+											className={style.images_footer}
+										/>
+									</a>
+								</span>
+							</div>
+						</form>
+					</div>
+				</div>
+
+				{/* 
         <div className={style.sousBlocInput}>
           <div className={style.sousBlocInputFlex}>
             <div className={style.inputMarge}>
@@ -307,9 +295,9 @@ const Contact = () => {
             </div>
           </div>
         </div> */}
-      </div>
-    </section>
-  );
+			</div>
+		</section>
+	);
 };
 
 export default Contact;
