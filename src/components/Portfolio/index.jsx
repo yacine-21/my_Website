ef3358d7d2ef3bcedc8d829768c96e2102168fd3
unknown <ylyoubi@gamingcampus.local>
2023-09-27 15:45:28 +0200

    start three mode for portfolio

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -1,34 +1,56 @@
-import React from "react";
-import style from "./index.module.css";
+import React,{useEffect} from 'react'
+import style from './index.module.css'
+import AvailableMode from '../AvailableMode'
 
 const Portfolio = () => {
-	return (
-		<>
-			{/* <div class={style.wrap}>
-        <div>
-          <h1>PROJET METEO JS VANILLA</h1>
+    const availableModeViews = [
+        {
+            index: 1,
+            label: 'CLASSIC',
+            image: 'neutral',
+            description:
+                'Experience a clean and professional design, showcasing a blend of traditional layouts and modern aesthetics. Perfect for those who appreciate elegance and simplicity.',
+        },
+        {
+            index: 2,
+            label: 'GAMING',
+            image: 'gaming',
+            description:
+                'Dive into a dynamic and immersive interface inspired by the world of gaming. Vibrant colors, interactive elements, and a touch of adrenaline await you.',
+        },
+        {
+            index: 3,
+            label: 'ETHICAL HACKER',
+            image: 'hacker',
+            description:
+                'Step into the realm of cybersecurity with a hacker-inspired theme. Dark, mysterious, and filled with encrypted codes and digital art. For the tech enthusiasts and security aficionados.',
+        },
+    ]
+	
+	useEffect(() => {
+	  // console.log("hey")
+	}, [])
+	
 
-          <a href="https://github.com/yacine-21/Projet_Meteo_JS_Vanilla">
-            Github
-          </a>
-          <a href="https://projet-meteo-js-vanilla.vercel.app/">Projet</a>
-        </div>
-        <div>B</div>
-        <div>C</div>
-        <div>D</div>
-        <div>E</div>
-      </div> */}
-			<div className={style.wrap}>
-				<h1>PORFOLIO COMMING SOON</h1>
-				<p>PLEASE COME BACK LATER</p>
-				<img
-					className={style.construction}
-					src='/images/construction.gif'
-					alt='construction'
-				/>
-			</div>
-		</>
-	);
-};
+    return (
+        <>
+            <div className={style.wrap}>
+                <p>PLEASE SELECT THE MODE :</p>
+                <section className={style.sectionContainer}>
+                    {availableModeViews.map((availableModeView) => {
+                        return (
+                            <AvailableMode
+                                description={availableModeView.description}
+                                image={availableModeView.image}
+                                mode={availableModeView.label}
+                                key={availableModeView.index}
+                            />
+                        )
+                    })}
+                </section>
+            </div>
+        </>
+    )
+}
 
-export default Portfolio;
+export default Portfolio
