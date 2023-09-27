66f3f014d7893f9c66e82b659fb7cc3fd224f69e
yacine-21 <yacinelyoubi37@gmail.com>
2023-09-27 21:23:20 +0200

    add redirect depending of the local storage for portfolio

--- src/components/AvailableMode.jsx
+++ src/components/AvailableMode.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 
-function AvailableMode({ mode, image, description }) {
+function AvailableMode({ mode,slug, image, description, handleSelectMode }) {
+    const handleClick = () => {
+        handleSelectMode(slug) // Pass the mode as an argument
+    }
+
     return (
         <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <img
                 class="rounded-t-lg"
                 src={`/images/illustration/${image}.png`}
-                alt="test"
+                alt="illustration"
             />
             <div class="p-5">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
@@ -16,8 +20,9 @@ function AvailableMode({ mode, image, description }) {
                     {description}
                 </p>
                 <a
+                    onClick={handleClick}
                     href="#"
-                    class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                     SELECT
                 </a>
@@ -26,4 +31,4 @@ function AvailableMode({ mode, image, description }) {
     )
 }
 
-export default AvailableMode
+export default AvailableMode // Memoize the component
---
+++ src/view/PortfolioHacking.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const PortfolioHacking = () => {
+  return (
+    <>
+      <Header />
+      <h1>
+        PORTFOLIO Hacking
+      </h1>
+      <Footer />
+    </>
+  );
+};
+
+export default PortfolioHacking;
